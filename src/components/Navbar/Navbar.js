05b1747a2a6644a88832370864ada19b6bde04b1
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-10-25 07:38:32 +0600

    Navbar logo added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavImg from "./icon4.png";
 
 const Navbar = () => {
   return (
@@ -8,7 +9,7 @@ const Navbar = () => {
         <div className="container-fluid">
           <Link className="navbar-brand">
             {" "}
-            <img src="./icon4.png" /> Educator
+            <img src={NavImg} style={{ width: 40, height: 34 }} /> Educator
           </Link>
           <button
             className="navbar-toggler"
