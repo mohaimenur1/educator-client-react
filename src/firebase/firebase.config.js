b80ebf2ac18b0bc1e7bf857a5001b4c58b75a920
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-10-26 12:45:12 +0600

    add firebase environment varibale

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCZQCPMMXBQRr9JCvYPSVPiH6FhWMWjvxg",
-  authDomain: "educator-fd15d.firebaseapp.com",
-  projectId: "educator-fd15d",
-  storageBucket: "educator-fd15d.appspot.com",
-  messagingSenderId: "490963747333",
-  appId: "1:490963747333:web:316e53d456be1de46f926c",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
