855b43af2bef11f21acb74ee3c034b694e407c6d
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-10-27 08:51:28 +0600

    checkout page design fixed

--- src/page/CheckOut/CheckOut.js
+++ src/page/CheckOut/CheckOut.js
@@ -6,6 +6,9 @@ const CheckOut = () => {
   return (
     <div className="">
       <h1 className="text-center m-5">{details.pragram_name}</h1>
+      <h3 className="text-center fs-3 fst-italic display-2">
+        Thank's For Getting Premium Access
+      </h3>
     </div>
   );
 };
